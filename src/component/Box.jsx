f8fc65b726501d3b2c83684f14739cde0d9928dd
YunheeAhn <ayh95944674@gmail.com>
2025-10-21 21:55:41 +0900

    feat : 유저와 컴퓨터 승패 결과 반대로 노출

--- src/component/Box.jsx
+++ src/component/Box.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 
 const Box = (props) => {
+  let result;
+
+  // 컴퓨터 박스일 때 승패 결과를 반대로 변경
+  if (props.title === "컴퓨터" && props.result !== "tie" && props.result !== "") {
+    result = props.result === "win" ? "lose" : "win";
+  } else {
+    result = props.result;
+  }
+  if (props.title === "컴퓨터") {
+    console.log("computer", result);
+  }
+
   return (
-    <div className="box">
+    <div className={`box ${result}`}>
       <h1>{props.title}</h1>
       <img src={props.item && props.item.img} alt="가위바위보" />
-      <p>{props.result}</p>
+      <p>{result}</p>
     </div>
   );
 };
